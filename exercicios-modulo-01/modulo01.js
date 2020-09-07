2166e2b44b97b93c31253da07e58824d41330282
Leandro Nunes <nunes.pessoal@gmail.com>
2020-09-07 12:24:30 -0300

    Module 1, exercise 1

---
+++ exercicios-modulo-01/modulo01.js
@@ -0,0 +1,14 @@
+var endereco = {
+    rua: "Rua dos Pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+   };
+
+function address(){
+    document.write(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com
+nº ${endereco.numero}`);
+}
+
+address();
